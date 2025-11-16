dd6eca24ddd74517ee9cc26b251f2e3fa446b895
Viktor Buzina <9362304@stud.op.edu.ua>
2025-11-16 18:42:41 +0200

    Lab7 is complete

---
+++ src/main/java/ua/opnu/Student.java
@@ -0,0 +1,36 @@
+package ua.opnu;
+
+import java.util.Arrays;
+
+public class Student {
+    private String name;
+    private String group;
+    private int[] marks;
+
+    public Student(String name, String group, int[] marks) {
+        this.name = name;
+        this.group = group;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", group='" + group + '\'' +
+                ", marks=" + Arrays.toString(marks) +
+                '}';
+    }
+}
